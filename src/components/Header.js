360936759aa23da73b68faadc393bc849afaced9
jeannot diambu <diambujeannot@gmail.com>
2023-09-28 12:30:07 +0100

    top

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import 'tailwindcss/tailwind.css';
 
 export default function Header() {
   return (
-    <div className='fixed left-0 right-0 bg-slate-50 px-4 pb-3 pt-3'>
+    <div className='fixed left-0 right-0 top-0 bg-slate-50 px-4 pb-3 pt-3'>
         <h3 className='font-bold text-20 text-[#564453] mb-2'>Chat</h3>
         <div className=''>
             <input type="text" placeholder="Search users..." className='outline-none py-2 px-2 w-full bg-[#e6ebf5] rounded-xl' />
